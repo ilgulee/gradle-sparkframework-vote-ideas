8b56368710341f7e262373e615c15265f81d9c40
Ilgu Lee <iglee2000ca@gmail.com>
2018-05-16 10:29:40 -0400

    Add spark framework using gradle

---
+++ src/main/java/com/ilgu/ideas/Main.java
@@ -0,0 +1,11 @@
+package com.ilgu.ideas;
+
+import static spark.Spark.get;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/hello",(req,res)->{
+            return "Hello Page";
+        });
+    }
+}
